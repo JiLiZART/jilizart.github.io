a5375ed0c4c378252dc03fe79cde9a99c82febf6
Nikolay Kost <JiLiZART@Gmail.com>
2025-03-30 23:43:38 +0200

    feat: add multi cursors (#1)
    * feat: migrate astro
    
    * fix: cursors

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 // import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
+
 import fs from "node:fs";
 
 const https = {
---
+++ src/helpers/t.ts
@@ -0,0 +1,8 @@
+
+export function t(key: string, options?: Record<string, string>) {
+    return key;
+}
+
+export function Trans(props: Record<string, string>) {
+    return props.children;
+}
